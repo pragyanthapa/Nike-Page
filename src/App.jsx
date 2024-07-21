708f806f1229a9ba86559ff30881b248edf0ef84
Pragyan Thapa <pragyanthapa44@gmail.com>
2024-07-21 20:51:24 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  Navigation  from './Component/Navigation'
+import './App.css'
+import HeroSection from './Component/HeroSection'
+
+function App() {
+  return (
+  <div>
+  <Navigation/>
+  <HeroSection/>
+</div>
+
+)  
+}
+export default App;
---
+++ src/Component/HeroSection.jsx
@@ -0,0 +1,30 @@
+const HeroSection = () =>{
+return <main className="hero container">
+    <div className="hero-content">
+        <h1>YOUR FEET DESERVES THE BEST</h1>
+        <p>YOUR FEET DESERVES THE BEST AND WE'RE HERE TO HELP YOU WITH OUR
+            SHOES. YOUR FEET DESERVES THE BEST AND WE'RE HERE TO HELP YOU WITH
+            OUR 
+            SHOES.
+        </p>
+        <div className="hero-button">
+        <button>Shop Now</button>
+        <button className="secondary-button">Category</button>
+        </div>
+        <div className="shopping">
+            <p>Available online on</p>
+            <div className="brand-icons">
+                <img src="./Images/amazon.png" alt="amazon-logo"/>
+                <img src="./Images/flipkart.png" alt="flipkart-logo"/>
+            </div>
+        </div>
+
+         </div>
+    <div className="hero-image"></div>
+    <img src="./Images/shoe_image.png" alt="shoe-logo"/>
+
+</main>
+    
+}
+
+export default HeroSection;
